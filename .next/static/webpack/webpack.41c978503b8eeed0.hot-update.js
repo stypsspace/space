7bbd5bf2dc0bfb850f5643fa54ed4aec8456403b
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-07-18 18:18:30 +0200

    Update

--- .next/static/webpack/webpack.41c978503b8eeed0.hot-update.js
+++ .next/static/webpack/webpack.41c978503b8eeed0.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ce95e59373f85268"; }
+/******/ 	__webpack_require__.h = function() { return "d34d160e9e79f97f"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
